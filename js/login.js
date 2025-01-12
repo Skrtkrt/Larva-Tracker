85edfed91a55596139b72aa092cc95f500afefff
skrtkrt <kurtsison137@gmail.com>
2025-01-12 09:58:18 +0800

    Initial commit

---
+++ js/login.js
@@ -0,0 +1,4 @@
+const loginForm = document.getElementById('loginForm');
+const usernameInput = document.getElementById('usernameInput');
+const passwordInput = document.getElementById('passwordInput');
+const errorMessage = document.getElementById('errorMessage');
---
+++ js/map.js
@@ -0,0 +1,1 @@
+const map = L.map('map').setView
